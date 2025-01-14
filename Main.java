d8c069c8a5966020c809e6a834f77a816aafda0a
Abián <abian.camejo@gmail.com>
2025-01-14 14:50:41 +0000

    Se creo en main una instancia de yoga, boxeo y funcional para demostrar que funciona

--- Main.java
+++ Main.java
@@ -3,5 +3,13 @@ public class Main {
         Gimnasio miGimnasio = new Gimnasio("FitLife Gym");
         miGimnasio.mostrarActividades();
         miGimnasio.registrarUsuario("Juan Perez");
+
+        Actividad yoga = new Yoga(3, 60);  
+        Actividad boxeo = new Boxeo(5, 45);  
+        Actividad funcional = new Funcional(4, 50);
+
+        yoga.mostrarInfo();
+        boxeo.mostrarInfo();
+        funcional.mostrarInfo();
     }
 }
