49c42616ee25f82e7d56739280d1819e7fa2c59b
Abián <abian.camejo@gmail.com>
2025-01-14 14:17:04 +0000

    Mejora: se añadió en Actividad las variables nombre, intensidad y duración junto con sus getter y setters, además, se creo un metodo abstracto mostrarinfo para que se sobreescriba en las clases boxeo, funciona y yoga

--- Actividad.java
+++ Actividad.java
@@ -1,3 +1,38 @@
-public class Actividad {
-    
+abstract class Actividad {
+    protected String nombre;
+    protected int intensidad;
+    protected int duracion;
+
+    public Actividad(String nombre, int intensidad, int duracion) {
+        this.nombre = nombre;
+        this.intensidad = intensidad;
+        this.duracion = duracion;
+    }
+
+    public abstract void mostrarInfo();
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIntensidad() {
+        return intensidad;
+    }
+
+    public void setIntensidad(int intensidad) {
+        this.intensidad = intensidad;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
 }
